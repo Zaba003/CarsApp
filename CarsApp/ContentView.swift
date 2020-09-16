7758ea65c755b32116337fcc01114cfa247c477d
Kirill Zaborsky <avto.oil@gmail.com>
2020-09-16 18:33:02 +0700

    Initial Commit

---
+++ CarsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarsApp
+//
+//  Created by Кирилл Заборский on 16.09.2020.
+//  Copyright © 2020 Кирилл Заборский. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
